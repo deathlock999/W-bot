fc4b586fb83c9adbe78a5bfffaa41126c350c8db
deathlock999 <166943020+deathlock999@users.noreply.github.com>
2024-04-29 12:44:39 +0530

    Create celery.py

---
+++ celery.py
@@ -0,0 +1,13 @@
+from celery import Celery
+from celery.schedules import crontab
+
+app = Celery('tasks', broker='amqp://localhost')
+
+# ... other Celery configuration options (if needed)
+
+CELERY_BEAT_SCHEDULE = {
+    'process_questions': {
+        'task': 'app.index',  # Replace 'your_app' with your actual module name
+        'schedule': crontab(minute='*/60'),  # Runs every 30 minutes
+    }
+}
